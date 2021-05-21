0cb21c628007ef94abbf19750b2ae9bb0ba06785
Duncan Mac-Vicar P <duncan@codenotary.com>
2021-05-21 19:02:13 +0200

    feat(webconsole): show a default page if console is not built-in.
    Instead of a 404, we show a simple default static webpage.
    For facilitating testing, / responds with a redirect to /missing.

--- webconsole/webconsole_default.go
+++ webconsole/webconsole_default.go
@@ -1,13 +1,25 @@
 // +build !webconsole
+//go:generate go run github.com/rakyll/statik -f -src=./default
 
 package webconsole
 
 import (
 	"github.com/codenotary/immudb/pkg/logger"
 	"net/http"
+	// embedded static files
+	_ "github.com/codenotary/immudb/webconsole/statik"
+	"github.com/rakyll/statik/fs"
 )
 
-func SetupWebconsole(_ *http.ServeMux, _ logger.Logger, _ string) error {
-	// no op
+func SetupWebconsole(mux *http.ServeMux, l logger.Logger, addr string) error {
+	statikFS, err := fs.New()
+	if err != nil {
+		return err
+	}
+	l.Infof("Webconsole not built-in")
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/missing/", http.StatusMovedPermanently)
+	})
+	mux.Handle("/missing/", http.FileServer(statikFS))
 	return nil
 }
